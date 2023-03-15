443efc14dc77b7867f8b0b9de07986bb39e1fa02
VaghelaSushma <123558131+VaghelaSushma@users.noreply.github.com>
2023-03-15 21:18:08 +0000

    Add files via upload

---
+++ Disha_Centre.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+public class Disha_Centre extends S2D {
+
+	@Override
+	public void manual_class() {
+		System.out.println("This must be delivered in the begining");
+		
+	}
+
+	@Override
+	public void api_class() {
+		System.out.println("This must be delivered at the end");
+		
+	}
+
+}
